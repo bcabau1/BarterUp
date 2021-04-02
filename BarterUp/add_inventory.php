4021e13284840e01d7887e79957e0e71d11db58c
bcabau1 <thelightserenade@gmail.com>
2021-04-02 19:50:58 -0400

    Update add_inventory.php
    forms update according to select option

--- BarterUp/add_inventory.php
+++ BarterUp/add_inventory.php
@@ -5,42 +5,26 @@ session_start();
 	include("functions.php");
 
 	$user_data = check_login($con);
-
-    #$sql = "insert into barterup.item values ();"
-	$showdiv = false;
 ?>
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-	toggleFields();
-	$("#cases").click(function () {
-			toggleFields();
-		});
 
-
-	function toggleFields() {
-		if ($("#cases").val() === "movie")	{
-			$("#movie").show()
-		}
-		if ($("#cases").val() === "music")	{
-			$("#music").show()
-		}
-		if ($("#cases").val() === "video_game")	{
-			$("#videogame").show()
-		}
-		else	{
-			$("#music").hide()
-			$("#movie").hide()
-			$("#videogame").hide()
-		}
-	}
-}
-</script>
 
 <!DOCTYPE html>
+
 <html lang="en">
 
+<head>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script async>
+		$(function(){
+			$('.form-group').hide();
+
+			$('#cases').change(function() {
+			$('.form-group').hide();
+			$('#' + $(this).val()).show();
+			})
+		});
+	</script>
 
 	<meta charset="UTF-8">
 	<title>Login</title>
@@ -69,33 +53,19 @@ $(document).ready(function(){
         </div>
 		<h1>Add to Inventory</h1>
         <form method="post">
-			<div class="form-group">
+			<div class="cat-group">
 				<label for="category">Category</label><br>
 				<select name="category" id="cases">
 					<option> Choose Category </option>
 					<option value="movie">Movie</option>
 					<option value="music">Music</option>
-					<option value="video_game">Video Game</option>
-					<option value="misc">Miscellaneous</option>
+					<option value="videogame">Video Game</option>
 				</select>
 			</div>
 			<div id="movie" class="form-group">
 				<label for="Item">Movie Title</label><br>
 				<input type="text" name="item_id" class="form-control">
-				
-            </div>
-			<div id="music" class="form-group">
-				<label for="Item">Album Name</label><br>
-				<input type="text" name="item_id" class="form-control"><br>
-				<label for="Item">Artist Name</label><br>
-				<input type="text" name="item_id" class="form-control">
-            </div>
-			<div id="videogame" class="form-group">
-				<label for="Item">Game Platform</label><br>
-				<input type="text" name="item_id" class="form-control">
-		
-            </div>
-			<div id="misc" class="form-group">
+				<div id="misc" class="misc-group">
 				<label for="Condition">Condition</label><br>
 				<select name="condition" id="condition">
 					<option value="excellent">Excellent</option>
@@ -103,10 +73,46 @@ $(document).ready(function(){
 					<option value="average">Average</option>
 					<option value="poor">Poor</option>
 				</select>
-				<br><br><label for="Category">Description</label><br>
+				<br><label for="Category">Description</label><br>
 				<textarea id="description" name="description" rows="4" cols="50">
                 </textarea>
             </div>
+            </div>
+			<div id="music" class="form-group">
+				<label for="Item">Album Name</label><br>
+				<input type="text" name="item_id" class="form-control"><br>
+				<label for="Item">Artist Name</label><br>
+				<input type="text" name="item_id" class="form-control">
+				<div id="misc" class="misc-group">
+				<label for="Condition">Condition</label><br>
+				<select name="condition" id="condition">
+					<option value="excellent">Excellent</option>
+					<option value="very nice">Very Nice</option>
+					<option value="average">Average</option>
+					<option value="poor">Poor</option>
+				</select>
+				<br><label for="Category">Description</label><br>
+				<textarea id="description" name="description" rows="4" cols="50">
+                </textarea>
+            </div>
+            </div>
+			<div id="videogame" class="form-group">
+				<label for="Item">Game Platform</label><br>
+				<input type="text" name="item_id" class="form-control">
+				<div id="misc" class="misc-group">
+				<label for="Condition">Condition</label><br>
+				<select name="condition" id="condition">
+					<option value="excellent">Excellent</option>
+					<option value="very nice">Very Nice</option>
+					<option value="average">Average</option>
+					<option value="poor">Poor</option>
+				</select>
+				<br><label for="Category">Description</label><br>
+				<textarea id="description" name="description" rows="4" cols="50">
+                </textarea>
+            </div>
+            </div>
+			
 		</form>
         <div id="center_button">
             <button onclick="location.href='inventory.php'">Add</button>

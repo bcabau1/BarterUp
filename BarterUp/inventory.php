3accf041fa69ad63f78690c0052c653db3db5794
TeddyBonkerz <radzlaif@gmail.com>
2021-04-06 22:20:37 -0400

    added page header and a few other shizz.

--- BarterUp/inventory.php
+++ BarterUp/inventory.php
@@ -1,67 +1,78 @@
-<?php 
+<?php
 session_start();
 
-	include("config.php");
-	include("functions.php");
+include("config.php");
+include("functions.php");
 
-	$user_data = check_login($con);
+$user_data = check_login($con);
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>BarterUp</title>
+    <title>BarterUp</title>
     <style>
-        table, th, td {
-        border-collapse: collapse;
-        border: 1px solid black;
-        width: 100%;
+        table,
+        th,
+        td {
+            border-collapse: collapse;
+            border: 1px solid black;
+            width: 100%;
         }
+
         table.center {
-        margin-left: auto;
-        margin-right: auto;
+            margin-left: auto;
+            margin-right: auto;
         }
+
         body {
-			font: 14px sans-serif;
-		}
+            font: 14px sans-serif;
+        }
+
         .wrapper {
-			width: 350px;
-			padding: 20px;
-			margin: 0 auto;
-		}
+            width: 350px;
+            padding: 20px;
+            margin: 0 auto;
+        }
+
         .welcome {
             float: right;
         }
     </style>
 </head>
-<body>
-<div class="wrapper">
-	<a href="index.php">Home Page</a>
-    <div class="welcome">
-    	Hello, <?php echo $user_data['username']; ?>
-    </div>
-	<h1>Inventory</h1>
 
-    <table>
-        <tr>
-            <th>Item</th>
-            <th>Category</th>
-            <th>Description</th>
-            <th>Item Condition</th>
-        </tr>
-        <tr>
-            <td>asdf </td>
-            <td> asdf</td>
-            <td> asdf</td>
-            <td> asdf</td>
-        </tr>
-        <tr>
-            <td> asdf</td>
-            <td> asdf</td>
-            <td>asdf </td>
-            <td> asdf</td>
-        </tr>
-    </table>
+<body>
+    <div class="container">
+        <?php include("./shared/header.php"); ?>
+        <div class="wrapper">
+            <div class="welcome">
+                Hello, <?php echo $user_data['username']; ?>
+            </div>
+            <h1>Inventory</h1>
+
+            <table>
+                <tr>
+                    <th>Item</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                    <th>Item Condition</th>
+                </tr>
+                <tr>
+                    <td>asdf </td>
+                    <td> asdf</td>
+                    <td> asdf</td>
+                    <td> asdf</td>
+                </tr>
+                <tr>
+                    <td> asdf</td>
+                    <td> asdf</td>
+                    <td>asdf </td>
+                    <td> asdf</td>
+                </tr>
+            </table>
+        </div>
     </div>
 </body>
+
 </html>
---
+++ BarterUp/shared/header.php
@@ -0,0 +1,18 @@
+
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="shared/header.css">
+</head>
+
+<div class="header">
+  <a href="#default" class="logo">BarterUp</a>
+  
+  <div class="header-right">
+    <a href="./current_trades.php">Home</a>
+    <a href='./inventory.php'>View Inventory</a>
+    <a href='./add_inventory.php'>Add to Inventory</a>
+    <a href="./logout.php">Logout</a>
+  </div>
+</div>
+
+</html>

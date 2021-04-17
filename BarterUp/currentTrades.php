b4f8d9150732466c33c6d0453b3646f9c56e6631
TeddyBonkerz <radzlaif@gmail.com>
2021-04-17 17:38:56 -0400

    got current pages going

--- BarterUp/currentTrades.php
+++ BarterUp/currentTrades.php
@@ -25,34 +25,61 @@ $user_data = check_login($con);
     </div>
     <div class="wrapper">
         <?php
-        $query = "select x.*, y.* from 
-        (SELECT user.username as initiator, item.item_name as initiator_item
-        FROM trades, user, item
-        where user.user_id = trades.user_id_initiator
-        and item.item_id = trades.item_id_initiator) as x, 
-        (SELECT user.username as tradee, item.item_name as tradee_item
-        FROM trades, user, item
-        where user.user_id = trades.user_id_tradee
-        and item.item_id = trades.item_id_tradee) as y;";
+        $query = "select * from trades";
         $result = mysqli_query($con, $query);
         if ($result && mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
+                $query1 = "select user.user_id as initiatorId, user.username as initiator, item.item_name as initiator_item
+                FROM trades, user, item
+                where user.user_id =" . $row['user_id_initiator'] . "
+                and item.item_id = " . $row['item_id_initiator'] . "";
+                $result1 = mysqli_query($con, $query1);
+                $row1 = mysqli_fetch_assoc($result1);
                 echo '
-            <ul class="list-view">
+                <ul class="list-view">
                 <li class="trader">
                     <p class="title">Trade Initiator</p>
-                    <p class="initiator-name">Name: '.$row['initiator'].'</p>
-                    <p class="initiator-item">Item Name:'.$row['initiator_item'].'</p>
-                </li>
+                    <p class="initiator-name">Name: ' . $row1['initiator'] . '</p>
+                    <p class="initiator-item">Item Name:' . $row1['initiator_item'] . '</p>
+                </li>';
 
+                $query2 = "select user.user_id as tradeeId, user.username as tradee, item.item_name as tradee_item
+                FROM trades, user, item
+                where user.user_id =" . $row['user_id_tradee'] . "
+                and item.item_id = " . $row['item_id_tradee'] . "";
+                $result2 = mysqli_query($con, $query2);
+                if ($result2 && mysqli_num_rows($result2) > 0) {
+
+                    $row2 = mysqli_fetch_assoc($result2);
+                    echo '
                 <li class="tradee">
                     <p class="title">Tradee</p>
-                    <p class="tradee-name">Name: '.($row['tradee'] == null ? "None":$row['tradee']).'</p>
-                    <p class="tradee-item">Item Name: '.($row['tradee_item'] == null ? "None":$row['tradee_item']).'</p>
+                    <p class="tradee-name">Name: ' . $row2['tradee'] . '</p>
+                    <p class="tradee-item">Item Name: ' . $row2['tradee_item'] . '</p>
                 </li>
-            </ul>';
+                </ul>';
+                } else {
+                    echo '
+                <li class="tradee">
+                    <p class="title">Trade an Item</p>
+                    <select>
+                        <option value="" disabled selected>Select an Item</option>' .
+                        $query3 = 'select * from item where user_id = ' . $_SESSION['user_id'] . ';';
+                    $result3 = mysqli_query($con, $query3);
+                    if ($result3 && mysqli_num_rows($result3) > 0) {
+                        while ($row3 = mysqli_fetch_assoc($result3)) {
+                            echo '<option value="item">' . $row3['item_name'] . '</option>';
+                        }
+                        echo '</select>';
+                        '
+                    </li>
+                </ul>';
+                    }
+                }
+                echo '<h1>IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII</h1>';
             }
-        } ?>
+        }
+        ?>
     </div>
 
     <script src="./shared/scripts.js"></script>

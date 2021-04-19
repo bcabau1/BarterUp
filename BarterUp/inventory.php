1a98a7abb0a323cc48ceab7e0642260cfbb51b57
bcabau1 <thelightserenade@gmail.com>
2021-04-18 22:58:13 -0400

    updated css

--- BarterUp/inventory.php
+++ BarterUp/inventory.php
@@ -46,11 +46,11 @@ $user_data = check_login($con);
                                 <p class="content">ID:'.$row['item_id'].'</p>
                             </div>
                             <div class="vi_middle">
-                                <h5>Condition:</h5>
+                                <h4>Condition:</h4>
                                 <p class="content">'.$row['item_condition'].'</p>
                             </div>
                             <div class="vi_right">
-                                <h5>Description:</h5>
+                                <h4>Description:</h4>
                                 <p class="content">'.$row['description'].'</p>
                             </div>'; 
                             if(mysqli_num_rows($result2) == 0) { echo '

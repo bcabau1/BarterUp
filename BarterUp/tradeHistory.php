de68d29525754016d298185be35adb5041fa2298
bcabau1 <thelightserenade@gmail.com>
2021-04-18 23:35:43 -0400

    Update tradeHistory.php

--- BarterUp/tradeHistory.php
+++ BarterUp/tradeHistory.php
@@ -24,7 +24,7 @@ $user_data = check_login($con);
         <div class="wrapper">
         <div class="view_main">
         <div class="page_name">
-        <h1>Trade History</h1>
+        <h1>My Trade History</h1>
         </div>
 
             <?php
@@ -67,61 +67,6 @@ $user_data = check_login($con);
             }
         }
         ?>
-
-
-
-
-               <?php /*
-                $query = "select * from trade_history where user_history_id_initiator = '".$_SESSION['user_id']."'
-                            UNION select * from trade_history where user_history_id_tradee =  '".$_SESSION['user_id']."';";
-                $result = mysqli_query($con, $query);
-                if ($result && mysqli_num_rows($result) > 0) {
-                     while($row= mysqli_fetch_assoc($result)) {
-                         if($row['user_history_id_initiator'] == $_SESSION['user_id'])   {
-                            echo 
-                            '<div class="view_item">
-                                <div class="vi_left">
-                                    <h5>As Initiator</h5>
-                                    <p class="content">Trade #'.$row['trade_id'].'</p>
-                                </div>
-                                <div class="vi_middle">
-                                    <h5>Traded With:</h5>
-                                    <p class="content">'.$row['user_history_id_tradee'].'</p>
-                                </div>
-                                <div class="vi_right">
-                                    <h5>Traded:</h5>
-                                    <p class="content">'.$row['item_history_id_initiator'].'</p>
-                                </div>
-                                <div class="vi_rightest">
-                                    <h5>For:</h5>
-                                    <p class="content">'.$row['item_history_id_tradee'].'</p>
-                                </div>
-                             </div>';
-                         }
-                         else if($row['user_history_id_tradee'] == $_SESSION['user_id']) {
-                            echo 
-                            '<div class="view_item">
-                                <div class="vi_left">
-                                    <h5>As Tradee</h5>
-                                    <p class="content">Trade #'.$row['trade_id'].'</p>
-                                </div>
-                                <div class="vi_middle">
-                                    <h5>Traded With:</h5>
-                                    <p class="content">'.$row['user_history_id_initiator'].'</p>
-                                </div>
-                                <div class="vi_right">
-                                    <h5>Traded:</h5>
-                                    <p class="content">'.$row['item_history_id_tradee'].'</p>
-                                </div>
-                                <div class="vi_rightest">
-                                    <h5>For:</h5>
-                                    <p class="content">'.$row['item_history_id_initiator'].'</p>
-                                </div>
-                             </div>';
-                         }
-                    }
-                }
-               */?>
             </div>
         </div>
     </div>
